87c68e5b9cc6b2dac0cd601845030a7b4cda1de8
JosukeH <solodcarlos@gmail.com>
2023-03-24 13:15:09 -0600

    angular

---
+++ src/app/module/clientes/clientes.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientesService } from './clientes.service';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.css']
+})
+
+
+export class ClientesComponent implements OnInit {
+  form = {
+    name :'',
+    email: '',
+  }
+
+  constructor(private clientesService : ClientesService){}
+  clientes: any[] = []
+
+  ngOnInit(): void {
+    this.getUsers()
+  }
+
+  getUsers(){
+    this.clientesService.getUsers().subscribe((data: any) => {
+      console.log(data);
+      
+      this.clientes = data
+    },
+    (error : any) => {
+      console.log(error);
+    }
+    )
+  }
+}
+
+  // selectIngrediente(data):void {
+  //   this.form = {
+  //     name :data.name,
+  //     email: data.email,
+  //     _id: data._id
+  //   }
+  //   console.log(this.form);
+  // }
+
+  // handleUpdate():void{
+  //   this._userService.updateUser(this.form).subscribe(data => {
+  //     console.log(data);
+  //     this.obtenerProductos()
+
+  //   }, 
+  //   error => {
+  //     console.log(error);
+      
+  //   })
+  // }  
+
---
+++ src/app/module/clientes/clientes.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+  Auth_Server: string = 'http://localhost:3000'
+  authSubject = new BehaviorSubject(false);
+  private token:string = '';
+
+  constructor( private httpClient: HttpClient) { }
+  getUsers() : any {
+    return this.httpClient.get(`${this.Auth_Server}/api/user`) 
+  }
+
+  updateUser(updateUser: any){
+    return this.httpClient.put(`${this.Auth_Server}/api/users`, updateUser) 
+  }
+
+  // login(user: any) : any{
+  //   return this.httpClient.post(`${this.Auth_Server}/api/users/login`, user) 
+  // }
+
+  register(user: any) : any{
+    return this.httpClient.post(`${this.Auth_Server}/api/users`, user) 
+  }
+
+
+  // logout(): void{
+  //   this.token = ''
+  //   localStorage.removeItem('ACCESS_TOKEN');
+  //   localStorage.removeItem('EXPIRES_IN');
+  // }
+
+  //  saveUser(user: any): void{
+  //     localStorage.setItem('user', JSON.stringify(user));
+  // }
+}
